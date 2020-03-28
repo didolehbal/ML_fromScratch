38d7658a84271d32caa917a8fcf5402ca378ebd1
didilehbal <habilmehdi96@gmail.com>
2020-03-28 04:12:22 +0100

    stop cond

--- Perceptron.py
+++ Perceptron.py
@@ -26,7 +26,7 @@ class Perceptron:
         return res
 
     def hw(self, x):
-        assert len(x) == len(self.weights) - 1 # -1 for b
+        assert len(x) == len(self.weights) - 1  # -1 for b
         input = x.copy()
         input.append(self.xb)
         res = self.scalar(input, self.weights)
@@ -35,24 +35,34 @@ class Perceptron:
 
 
 def train_perceptron(p):
-    biais = 0.1
+    alpha = 0.1
     training_data = [
         {"input": [2, 0], "output":1},
         {"input": [0, 3], "output":0},
         {"input": [3, 0], "output":0},
         {"input": [1, 1], "output":1},
     ]
-    for data in training_data:
-        r = p.hw(data["input"])
-        if(r != data["output"]):
-            #print("correcting with " + str(data))
-            for i in range(0, len(p.weights) - 1):
-                p.setW(i, p.weights[i] + biais *
-                       (data["output"] - r) * data["input"][i])
-            p.setW(i+1, p.weights[i+1] + biais * (data["output"] - r) * 1)
+
+    errors = True
+    counter = 0
+    maxIterations = 100
+
+    while errors and counter < maxIterations:
+        errors = False
+        counter += 1
+        for data in training_data:
+            r = p.hw(data["input"])
+            if(r != data["output"]):
+                errors = True
+                #print("correcting with " + str(data))
+                for i in range(0, len(p.weights) - 1):
+                    p.setW(i, p.weights[i] + alpha *
+                           (data["output"] - r) * data["input"][i])
+                p.setW(i+1, p.weights[i+1] + alpha * (data["output"] - r) * 1)
 
 
+# calls
 p = Perceptron([1, 0.5], [0, 0])
 train_perceptron(p)
 print(p.toString())
-print(p.hw([1, 1]))
+print(p.hw([2, 0]))
